ec55e277e59a92030ae7fce2cb4f477c9171c938
JetLearnAdhayan <adhayanraj9@gmail.com>
2024-09-09 17:31:31 +0100

    main.py

--- main.py
+++ main.py
@@ -38,3 +38,21 @@ while True:
     ret,img = webcam.read()
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(grayscale,1.3,5) 
+    for (x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        face = grayscale[y:y + h, x:x + w]
+        face_resize = cv2.resize(face,(width,height))
+        prediction = recogniser.predict(face_resize)
+        print(prediction)
+        if prediction[1]<500:
+            cv2.putText(img,"%s - %.0f" %(names[prediction[0]], prediction[1]), (x-10 , y-10), cv2.FONT_HERSHEY_PLAIN, 1,(0,255,0))
+        else:
+            cv2.putText(img, "not recognised" ,(x-10, y-10), cv2.FONT_HERSHEY_COMPLEX, 1,(0,255,0))
+             
+         
+    cv2.imshow("OpenCV", img) 
+
+    key = cv2.waitKey(10)
+    #space key
+    if key == 27:
+        break
